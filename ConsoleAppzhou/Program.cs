2a64ca89b1f6479664371a287c432c34fc6395ce
v-yuwzh <v-yuwzh@VYUWZH001RBB>
2020-02-19 12:45:11 +0000

    Add project files.

---
+++ ConsoleAppzhou/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppzhou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+        }
+    }
+}
